258ce9a5e4b57ca923734e548c399051d007b830
Kashinka <azalena@yandex.ru>
2023-05-10 15:40:47 +0300

    Added function correctName

--- index.js
+++ index.js
@@ -6,12 +6,20 @@ const form = document.getElementById('form');
 
 const fio = document.getElementById('fio');
 
+const fio3 = function correctName() {
+
+    let fio2 = fio.value;
+
+    return fio2[0].value.toUpperCase() + fio2.value.slice(1).toLowerCase;
+
+};
+
 const ava = document.getElementById('ava');
 
 const comment = document.getElementById('comment');
 
 button.onclick = function() {
 
-    result.innerHTML = fio.value + "\n" + ava.value + "\n" + comment.value;
+    result.innerHTML = fio3 + "\n" + ava.value + "\n" + comment.value;
 
 };
