58d92d1fab85a12e8129df92b99ab12bdf5482e7
Kashinka <azalena@yandex.ru>
2023-05-16 21:03:32 +0300

    Changed functions

--- index.js
+++ index.js
@@ -6,11 +6,27 @@ const form = document.getElementById('form');
 
 const fio = document.getElementById('fio');
 
+const fio2 = fio.value;
+
+const fio2_1 = function reg1() {
+
+    return fio2[0].toUpperCase();
+
+};
+
+const fio2_2 = function reg2() {
+    
+    return fio2.slice(1).toLowerCase();
+
+}
+
+const fio3_1 = fio2_1().replace(/\s+/g, '');
+
+const fio3_2 = fio2_2().replace(/\s+/g, '');
+
 let fio3 = function correctName() {
 
-    let fio2 = fio.value;
-
-    return fio2[0].toUpperCase().replace(/\s+/g, '') + fio2.slice(1).toLowerCase().replace(/\s+/g, '');
+    return fio3_1 + fio3_2;
 
 };
 
